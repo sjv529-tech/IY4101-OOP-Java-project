735c8ec0fa485e503a44c0d622225a935e41ec00
Ella Lou <sjv529@york.ac.uk>
2026-03-09 14:39:48 +0000

    initialise class

--- Main/src/coordinates.java
+++ Main/src/coordinates.java
@@ -2,15 +2,31 @@ public class coordinates {
     int x;
     int y;
 
-    public int getY(){
+    private int getY(){
         return y;
     }
 
-    public int getX(){
+    private int getX(){
         return x;
     }
 
-    public double distance(){
-
+    private double distance(int x1, int x2, int y1, int y2){
+        return Math.sqrt(Math.pow(2, (x2-x1)) + Math.pow(2, y2-y1));
     }
+
+    public void translate(int dx, int dy){
+        x += dx;
+        y += dy;
+    }
+
+    private void scale(bool method, float scale){
+        if (bool){
+            x = x*scale;
+            y = y*scale;
+        }else {
+            x = x/scale;
+            y = y/scale;
+        }
+    }
+
 }
--- Main/src/shape.java
+++ Main/src/shape.java
@@ -1,2 +1,15 @@
 public class shape {
+    private coordinates;
+    int sides;
+
+    private int getCoordinates extends coordinates{
+        return x;
+        return y;
+    }
+
+    private vint getSides(shape){
+        return sides;
+    }
+
+    private void setCoordinates()
 }
