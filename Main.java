9406ed3051d31fb5d35d8c0d8f2abb822971a5e4
Ella Lou <sjv529@york.ac.uk>
2026-03-30 14:17:27 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 class Main{
+    // Program entry point - creates manager and starts the application
     public static void main(String[] args) {
         shapeManage app = new shapeManage();
-        app.run();
+        app.run();  // Begin the interactive menu loop
     }
 }
